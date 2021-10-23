e80718e26f9f1f6c68d4fd7cfd6a33dda196505a
Horacio <74342359+SquareBoey@users.noreply.github.com>
2021-10-23 05:42:27 -0500

    Demuestran que tenemos Numpy y Pandas en nuestro Repositorio
    aaaaa

---
+++ Clase_de_EstrucbaseDat2021/EjerPanda.py
@@ -0,0 +1,4 @@
+import pandas as pd 
+import numpy as np 
+s = pd.Series(['matematicas','fisica','ingles','quimica','español'])
+print(s)
---
+++ Clase_de_EstrucbaseDat2021/TrabajosDeNumpyyy.py
@@ -0,0 +1,1 @@
+#documento parcialmente vacio que planeo llenar cuando haga las cosas de Numpy #
